ac2c50a553d69fec383e8a7f48060a863e07fc86
Kenneth Massada <kmassada@gmail.com>
2015-01-26 08:38:02 -0500

    change ga code

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -3,6 +3,6 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-58954022-1', 'auto');
+  ga('create', 'UA-58957717-1', 'auto');
+  ga('require', 'displayfeatures');
   ga('send', 'pageview');
-
